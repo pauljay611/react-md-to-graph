4d00d24f3077089eff055ed77f9ed36a4e787dda
pauljay611 <pauljay611@gmail.com>
2020-10-11 15:26:08 +0800

    fix side effects

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-require("./style/global.css");
+import "./style/global.css";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("app"));
