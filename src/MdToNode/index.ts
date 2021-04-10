31a8dbbe8d466980365a91d68abce126ce1caad2
pauljay611 <pauljay611@gmail.com>
2021-04-10 15:33:01 +0800

    fix types error

--- src/MdToNode/index.ts
+++ src/MdToNode/index.ts
@@ -1,20 +1,20 @@
-import { INode, IEdge } from 'react-digraph'
-import { v4 as uuidv4 } from 'uuid'
+import { INode, IEdge } from "react-digraph";
+import { v4 as uuidv4 } from "uuid";
 
-import { getTextNode, getTextEdge, filteredIllegalText } from './helpers'
-import { Node, NodeProps } from '../types'
-import { IGraphConfig } from '../utils/graphConfig'
+import { getTextNode, getTextEdge, filteredIllegalText } from "./helpers";
+import { EdgeProps, Node, NodeProps } from "../types";
+import { IGraphConfig } from "../utils/graphConfig";
 
 interface IMdToGraphNode {
-	getAllNodes<T>(tagName: string): Node<T>[]
-	transGraphNode(): IGraphNode
+  getAllNodes<T>(tagName: string): Node<T>[];
+  transGraphNode(): IGraphNode;
 }
 
 interface IGraphNode {
-	GraphConfig: IGraphConfig
-	nodes: INode[]
-	edges: IEdge[]
-	nodesMap: Map<string, INode>
+  GraphConfig: IGraphConfig;
+  nodes: INode[];
+  edges: IEdge[];
+  nodesMap: Map<string, INode>;
 }
 /**
  * change markdown to graph with nodes and edges
@@ -24,64 +24,64 @@ interface IGraphNode {
  * @param {IGraphConfig} graphConfig - graphConfig
  */
 export default class MdToGraphNode implements IMdToGraphNode {
-	constructor(
-		private mdText: string,
-		private tagNames: string[],
-		private edgeNames: string[],
-		private graphConfig: IGraphConfig
-	) {
-		this.mdText = mdText
-		this.tagNames = tagNames
-		this.edgeNames = edgeNames
-		this.graphConfig = graphConfig
-	}
+  constructor(
+    private mdText: string,
+    private tagNames: string[],
+    private edgeNames: string[],
+    private graphConfig: IGraphConfig
+  ) {
+    this.mdText = mdText;
+    this.tagNames = tagNames;
+    this.edgeNames = edgeNames;
+    this.graphConfig = graphConfig;
+  }
 
-	getAllNodes<T>(tagName: string, props?: NodeProps<T>) {
-		return getTextNode(this.mdText, tagName, props)
-	}
+  getAllNodes<T>(tagName: string, props?: NodeProps<T>) {
+    return getTextNode(this.mdText, tagName, props);
+  }
 
-	getAllEdges<T>(
-		tagName: string,
-		nodesMap: Map<string, INode>,
-		props?: NodeProps<T>
-	) {
-		return getTextEdge(this.mdText, tagName, props, nodesMap)
-	}
+  getAllEdges<T>(
+    tagName: string,
+    nodesMap: Map<string, INode>,
+    props?: EdgeProps<T>
+  ) {
+    return getTextEdge(this.mdText, tagName, props, nodesMap);
+  }
 
-	transGraphNode() {
-		const nodes: INode[] = this.tagNames
-			.map((name) =>
-				this.getAllNodes<string>(name).map((node) => ({
-					id: uuidv4(),
-					type: filteredIllegalText(node.type),
-					title: node.id,
-					props: { ...node.props, textContent: node.textContent }
-				}))
-			)
-			.reduce((acc, value) => [...acc, ...value], [])
+  transGraphNode() {
+    const nodes: INode[] = this.tagNames
+      .map((name) =>
+        this.getAllNodes<string>(name).map((node) => ({
+          id: uuidv4(),
+          type: filteredIllegalText(node.type),
+          title: node.id,
+          props: { ...node.props, textContent: node.textContent },
+        }))
+      )
+      .reduce((acc, value) => [...acc, ...value], []);
 
-		const nodesMap = new Map<string, INode>(
-			nodes.map((node) => [node.title, node])
-		)
+    const nodesMap = new Map<string, INode>(
+      nodes.map((node) => [node.title, node])
+    );
 
-		const edges: IEdge[] = this.edgeNames
-			.map((name) => {
-				return this.getAllEdges<string>(name, nodesMap).map((edge) => {
-					return {
-						type: edge.type,
-						source: filteredIllegalText(edge.source),
-						target: filteredIllegalText(edge.target),
-						props: { ...edge.props, textContent: edge.textContent }
-					}
-				})
-			})
-			.reduce((acc, value) => [...acc, ...value], [])
+    const edges: IEdge[] = this.edgeNames
+      .map((name) => {
+        return this.getAllEdges<string>(name, nodesMap).map((edge) => {
+          return {
+            type: edge.type,
+            source: filteredIllegalText(edge.source),
+            target: filteredIllegalText(edge.target),
+            props: { ...edge.props, textContent: edge.textContent },
+          };
+        });
+      })
+      .reduce((acc, value) => [...acc, ...value], []);
 
-		return {
-			GraphConfig: this.graphConfig,
-			nodes,
-			edges,
-			nodesMap
-		}
-	}
+    return {
+      GraphConfig: this.graphConfig,
+      nodes,
+      edges,
+      nodesMap,
+    };
+  }
 }
